8d374815cece53c83bf3a9ab53ddf983cb357a9c
Ricardo Avila <ravila@protonmail.com>
2020-10-15 22:51:57 -0700

    Fix indentation

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 def get_release(self):
     import requests
 
-release_summary = requests.get("http://current.geneontology.org/summary.txt")
+    release_summary = requests.get("http://current.geneontology.org/summary.txt")
     for line in release_summary.text.split("\n"):
         if line.startswith("Start date:"):
             version = line.split(": ")[-1]
