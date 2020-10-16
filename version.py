a6863a18352d61236af850adbb61a1163550bdb0
Ricardo Avila <ravila@protonmail.com>
2020-10-15 22:45:58 -0700

    Put import statement inside function

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
-import requests
-
 def get_release(self):
-    release_summary = requests.get("http://current.geneontology.org/summary.txt")
+    import requests
+
+release_summary = requests.get("http://current.geneontology.org/summary.txt")
     for line in release_summary.text.split("\n"):
         if line.startswith("Start date:"):
             version = line.split(": ")[-1]
