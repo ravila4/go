0275788769264597794274cd6509b7a664a927ef
Ricardo Avila <ravila@protonmail.com>
2020-12-08 10:27:23 -0700

    Move id lookup functionality to helper class

--- parser.py
+++ parser.py
@@ -2,9 +2,8 @@ import glob
 import json
 import os
 
-from biothings.utils.dataload import alwayslist
 from biothings.utils.dataload import tabfile_feeder, dict_sweep, unlist
-import mygene
+from .geneset_utils import IDLookup
 
 
 def load_data(data_folder):
@@ -26,21 +25,20 @@ def load_data(data_folder):
         uniprot = [i for i, j in all_genes]
         symbols = [j for i, j in all_genes]
         taxid = annotations['taxid']
-        genecache = get_gene_ids(symbols, uniprot, taxid)
+        lookup = IDLookup(taxid)
+        lookup.query_mygene(uniprot, "uniprot")
+        lookup.retry_failed_with_new_ids(symbols, "symbol")
 
         for _id, annotations in docs.items():
             # Add ontology annotations
             annotations['go'] = goterms[_id]
             # Add gene sets
             if annotations.get("genes") is not None:
-                genes = []
                 for u, s in annotations['genes']:
-                    if genecache.get(u) is not None:
-                        genes += [g for g in genecache[u].values()]
-                    elif genecache.get(s) is not None:
-                        genes += [g for g in genecache[s].values()]
-                    #else:
-                    #    genes += {'symbol': s, 'uniprot': u}
+                    if lookup.query_cache.get(u) is not None:
+                        genes = lookup.query_cache[u]
+                    elif lookup.query_cache.get(s) is not None:
+                        genes = lookup.query_cache[s]
                 annotations['genes'] = genes
             else:
                 # No genes in set
@@ -51,67 +49,15 @@ def load_data(data_folder):
                 if annotations.get(key) is not None:
                     genes = []
                     for u, s in annotations.pop(key):
-                        if genecache.get(u) is not None:
-                            genes += [g for g in genecache[u].values()]
-                        elif genecache.get(s) is not None:
-                            genes += [g for g in genecache[s].values()]
-                        #else:
-                        #    genes += {'symbol': s, 'uniprot': u}
+                        if lookup.query_cache.get(u) is not None:
+                            genes = lookup.query_cache[u]
+                        elif lookup.query_cache.get(s) is not None:
+                            genes = lookup.query_cache[s]
                     annotations['go'][key] = genes
             # Clean up data
             annotations = unlist(annotations)
+            annotations = dict_sweep(annotations)
             yield annotations
-
-
-def get_gene_ids(symbols, uniprot_ids, taxid):
-    """Fetch NCBI, Ensembl, and gene names from UniProt ids or gene symbol."""
-    mg = mygene.MyGeneInfo()
-    fields = 'entrezgene,ensembl.gene,name,symbol'
-    # Fetch ids from  UniProt
-    response = mg.querymany(uniprot_ids, scopes='uniprot', fields=fields,
-                            species=taxid, returnall=True)
-    genes = {}
-    for out in response['out']:
-        if out.get("_id") is not None:
-            query = out['query']
-            geneid = out['_id']
-            hits = genes.setdefault(query, {})
-            hits[geneid] = {"mygene_id": geneid,
-                            "uniprot": query,
-                            "symbol": out.get('symbol'),
-                            "name": out.get('name')}
-            if out.get("entrezgene") is not None:
-                hits[geneid].setdefault('ncbigene', [])
-                if out['entrezgene'] not in hits[geneid]['ncbigene']:
-                    hits[geneid]['ncbigene'].append(out['entrezgene'])
-            if out.get("ensembl") is not None:
-                hits[geneid].setdefault('ensemblgene', [])
-                hits[geneid]['ensemblgene'] = hits[geneid]['ensemblgene'] + \
-                    [i['gene'] for i in alwayslist(out['ensembl'])]
-    # Retry missing using gene symbol
-    retry = [symbols[uniprot_ids.index(k)] for k in response['missing']]
-    response = mg.querymany(retry, scopes='symbol', fields=fields,
-                            species=taxid, returnall=True)
-    for out in response['out']:
-        if out.get("_id") is not None:
-            query = out['query']
-            geneid = out['_id']
-            hits = genes.setdefault(query, {})
-            hits[geneid] = {"mygene_id": geneid,
-                            "uniprot": uniprot_ids[symbols.index(query)],
-                            "symbol": out['symbol'],
-                            "name": out.get('name')}
-            if out.get("entrezgene") is not None:
-                hits[geneid].setdefault('ncbigene', [])
-                if out['entrezgene'] not in hits[geneid]['ncbigene']:
-                    hits[geneid]['ncbigene'].append(out['entrezgene'])
-            if out.get("ensembl") is not None:
-                hits[geneid].setdefault('ensemblgene', [])
-                hits[geneid]['ensemblgene'] = hits[geneid]['ensemblgene'] + \
-                    [i['gene'] for i in alwayslist(out['ensembl'])]
-    genes = unlist(genes)
-    genes = dict_sweep(genes, vals=[None, 'null'])
-    return genes
 
 
 def parse_gene_annotations(f):
@@ -162,8 +108,9 @@ def parse_ontology(f):
             continue
         go_terms[_id] = {"id": _id.replace("_", ":"),
                          "url": url,
-                         "name": node.get('lbl'),
                          "type": node.get('type')}
+        if node.get('lbl'):
+            go_terms['name'] = node['lbl']
         if node['meta'].get("definition"):
             go_terms[_id]['definition'] = node['meta']['definition'].get('val')
             go_terms[_id]['xrefs'] = node['meta']['definition'].get('xrefs')
@@ -175,5 +122,6 @@ def parse_ontology(f):
 if __name__ == "__main__":
 
     annotations = load_data("./test_data")
-    for a in annotations:
-        print(json.dumps(a, indent=2))
+    with open("out.json", 'a') as outfile:
+        for a in annotations:
+            outfile.write(json.dumps(a, indent=2))
